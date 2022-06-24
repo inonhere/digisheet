c1f7f3632708f775f621c9766d1112d61769998d
Inon <pokemonxdloquendo@gmail.com>
2022-06-24 19:48:01 -0400

    add first stages

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const baby = document.querySelector('#baby')
+const baby2 = document.querySelector('#baby2')
 const digi = document.querySelector('#digi');
 const digi2 = document.querySelector('#digi2');
 const digi3 = document.querySelector('#digi3');
@@ -8,6 +10,24 @@ const digiD2 = document.querySelector('#digiD2');
 const digiD3 = document.querySelector('#digiD3');
 const digiD4 = document.querySelector('#digiD4');
 var upload = "";
+
+baby.addEventListener('change', function(){
+    const lectura = new FileReader();
+    lectura.addEventListener('load', ()  =>{
+        upload = lectura.result;
+        document.querySelector('#displayBaby').style.backgroundImage = `url(${upload})`
+    });
+    lectura.readAsDataURL(this.files[0])
+})
+
+baby2.addEventListener('change', function(){
+    const lectura = new FileReader();
+    lectura.addEventListener('load', ()  =>{
+        upload = lectura.result;
+        document.querySelector('#displayBaby2').style.backgroundImage = `url(${upload})`
+    });
+    lectura.readAsDataURL(this.files[0])
+})
 
 digi.addEventListener('change', function(){
     const lectura = new FileReader();
@@ -89,3 +109,4 @@ digiD4.addEventListener('change', function(){
     });
     lectura.readAsDataURL(this.files[0])
 })
+
